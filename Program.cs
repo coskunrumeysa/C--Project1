ba524d338b4137741f99d4fd7554a3cd8e1225c6
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-05-26 15:19:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//Beni Yaratan Elbet Yolumu Gösterir .Şuara78
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World!");
+            Console.Read();
+            
+        }
+    }
+}
